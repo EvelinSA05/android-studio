0f308caf3dcf09ead2975e8b3b6b35eda4ce64a0
EvelinSA05 <evelinsalsabila27@gmail.com>
2023-11-22 19:15:47 +0700

    android studio bagian 15

---
+++ android-studio-bag15/app/src/main/java/com/komputerkit/intentactivity/Penjumlahan.java
@@ -0,0 +1,14 @@
+package com.komputerkit.intentactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Penjumlahan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penjumlahan);
+    }
+}
